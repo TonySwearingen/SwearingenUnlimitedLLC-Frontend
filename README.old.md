e39048f9f23ad204c01a3106b421c0cf28c7ed31
TonySwearingen <unlimited1974@aol.com>
2022-04-28 18:08:47 -0500

    Auth0 working so far

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# SwearingenUnlimitedLLC-Frontend
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Login from './Login';
+import Logout from './Logout';
+import Profile from './Profile';
+
+function Home() {
+  return (
+    <div>
+      Home
+    <Login />
+    <Logout />
+    <Profile />
+    </div>
+  )
+}
+
+export default Home;
