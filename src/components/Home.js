345745eb8186816958cb28a951634f2c91675fc8
TonySwearingen <unlimited1974@aol.com>
2022-04-29 14:34:28 -0500

    Auth0 is working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import Login from './Login';
-import Logout from './Logout';
+import React, { useState, useEffect } from 'react';
 import Profile from './Profile';
+import NavBar from './NavBar';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Home() {
+
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  const [isUser,setIsUser] = useState([]);
+
+  useEffect(() => {
+  if (isAuthenticated){
+    fetch(`http://localhost:3001/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: user.nickname,
+        manager: false
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setIsUser(data))
+    
+  }}, [user])
+
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <div>
-      Home
-    <Login />
-    <Logout />
-    <Profile />
+      <NavBar isAuthenticated={isAuthenticated} />
+      <Profile isAuthenticated={isAuthenticated} user={user} />
     </div>
   )
 }
