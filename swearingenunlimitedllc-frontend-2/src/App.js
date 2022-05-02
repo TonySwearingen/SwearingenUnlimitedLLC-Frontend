1dc1cb8cf78ccd1dbc130b769af99a142471e381
TonySwearingen <unlimited1974@aol.com>
2022-05-02 18:12:57 -0500

    Initial Commit take 2

---
+++ swearingenunlimitedllc-frontend-2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Swearingen Unlimited LLC</h1>
+      <br />
+      <br />
+      <h2>By Tony J. Swearingen</h2>
+    </div>
+  );
+}
+
+export default App;
